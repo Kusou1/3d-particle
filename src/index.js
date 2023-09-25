aa30dba14a5f9de518f1c7b028668d6ed8e24850
Kusou1 <zhangshaohong1997@gmail.com>
2023-09-25 23:27:29 +0800

    Particle Geometry & Material

--- src/index.js
+++ src/index.js
@@ -1,70 +1,62 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Model from './model';
-
+import Model from './model'
+import { MeshSurfaceSampler } from 'three/examples/jsm/math/MeshSurfaceSampler'
 /*------------------------------
 Renderer
 ------------------------------*/
 const renderer = new THREE.WebGLRenderer({
-  antialias: true,
-  alpha: true
-});
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
+    antialias: true,
+    alpha: true
+})
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
 /*------------------------------
 Scene & Camera
 ------------------------------*/
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 
-  50, 
-  window.innerWidth / window.innerHeight,
-  0.1,
-  100
-);
-camera.position.z = 5;
-camera.position.y = 1;
-
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 100)
+camera.position.z = 5
+camera.position.y = 1
 
 /*------------------------------
 Mesh
 ------------------------------*/
 // const geometry = new THREE.BoxGeometry(2, 2, 2);
-// const material = new THREE.MeshBasicMaterial( { 
+// const material = new THREE.MeshBasicMaterial( {
 //   color: 0x00ff00,
 // } );
 // const cube = new THREE.Mesh( geometry, material );
 // scene.add( cube );
 
-
 /*------------------------------
 OrbitControls
 ------------------------------*/
-const controls = new OrbitControls( camera, renderer.domElement );
-
+const controls = new OrbitControls(camera, renderer.domElement)
 
 /*------------------------------
 Helpers
 ------------------------------*/
-const gridHelper = new THREE.GridHelper( 10, 10 );
-scene.add( gridHelper );
-const axesHelper = new THREE.AxesHelper( 5 );
-scene.add( axesHelper );
+// const gridHelper = new THREE.GridHelper( 10, 10 );
+// scene.add( gridHelper );
+// const axesHelper = new THREE.AxesHelper( 5 );
+// scene.add( axesHelper );
 
 /*------------------------------
 Models
 ------------------------------*/
 const skull = new Model({
-  name: 'skull',
-  file: './models/skull.glb',
-  scene: scene
+    name: 'skull',
+    file: './models/skull.glb',
+    scene: scene,
+    placeOnLoad: true
 })
 
 const horse = new Model({
-  name: 'horse',
-  file: './models/horse.glb',
-  scene: scene
+    name: 'horse',
+    file: './models/horse.glb',
+    scene: scene
 })
 
 /*------------------------------
@@ -80,39 +72,37 @@ const buttons = document.querySelectorAll('.button')
 //   horse.add()
 // })
 
-buttons[0].addEventListener("click",()=>{
-  skull.add()
-  horse.remove()
-  dragons.remove()
-  skulptur.remove()
-  tombstone.remove()
+buttons[0].addEventListener('click', () => {
+    skull.add()
+    horse.remove()
+    dragons.remove()
+    skulptur.remove()
+    tombstone.remove()
 })
 
-buttons[1].addEventListener("click",()=>{
-  horse.add()
-  skull.remove()
-  dragons.remove()
-  skulptur.remove()
-  tombstone.remove()
-
+buttons[1].addEventListener('click', () => {
+    horse.add()
+    skull.remove()
+    dragons.remove()
+    skulptur.remove()
+    tombstone.remove()
 })
 
 /*------------------------------
 Loop
 ------------------------------*/
 const animate = function () {
-  requestAnimationFrame( animate );
-  renderer.render( scene, camera );
-};
-animate();
-
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+}
+animate()
 
 /*------------------------------
 Resize
 ------------------------------*/
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
 }
-window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener('resize', onWindowResize, false)
--- src/model.js
+++ src/model.js
@@ -1,43 +1,79 @@
 import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import { MeshSurfaceSampler } from 'three/examples/jsm/math/MeshSurfaceSampler'
 
 class Model {
-    constructor (obj) {
+    constructor(obj) {
         this.name = obj.name
         this.file = obj.file
         this.scene = obj.scene
+        this.placeOnLoad = obj.placeOnLoad
 
         this.loader = new GLTFLoader()
         this.dracoLoader = new DRACOLoader()
         this.dracoLoader.setDecoderPath('./draco/')
-        this.loader.setDRACOLoader( this.dracoLoader )
+        this.loader.setDRACOLoader(this.dracoLoader)
 
         this.init()
     }
 
     init() {
         this.loader.load(this.file, (response) => {
-            console.log(response)
-
+            /*------------------------------
+            Original Mesh
+            ------------------------------*/
             this.mesh = response.scene.children[0]
-            console.log(this.mesh)
-            this.material = new THREE.MeshBasicMaterial({
-                color: "#fff768",
+            /*------------------------------
+            Particles Material
+            ------------------------------*/
+            this.particlesMaterial = new THREE.PointsMaterial({
+                color: '#fff768',
+                size: 0.02,
                 wireframe: true
             })
-
             this.mesh.material = this.material
-            console.log(this.mesh)
+
+            /*------------------------------
+            Particles Geometry
+            ------------------------------*/
+            const sampler = new MeshSurfaceSampler(this.mesh).build()
+            const numParticles = 10000
+            this.particlesGeometry = new THREE.BufferGeometry()
+            const particlesPosition = new Float32Array(numParticles * 3)
+
+            for (let i = 0; i < numParticles; i++) {
+                const newPosition = new THREE.Vector3()
+                sampler.sample(newPosition)
+
+                particlesPosition.set([newPosition.x, newPosition.y, newPosition.z], i * 3)
+            }
+
+            this.particlesGeometry.setAttribute('position', new THREE.BufferAttribute(particlesPosition, 3))
+
+            /*------------------------------
+            Geometry Mesh
+            ------------------------------*/
+            this.geometry = this.mesh.geometry
+
+            /*------------------------------
+            Particles
+            ------------------------------*/
+            this.particles = new THREE.Points(this.particlesGeometry, this.particlesMaterial)
+
+            // place on load
+            if (this.placeOnLoad) {
+                this.add()
+            }
         })
     }
-    
+
     add() {
-        this.scene.add(this.mesh)
+        this.scene.add(this.particles)
     }
 
     remove() {
-        this.scene.remove(this.mesh)
+        this.scene.remove(this.particles)
     }
 }
 
